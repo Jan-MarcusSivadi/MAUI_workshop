b135ea8dda4829eae1e4b0291dc3be1aa69e841c
Jan-MarcusSivadi <jmarcus@hot.ee>
2023-05-13 23:28:46 +0300

    Experimental style changes

--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -15,8 +15,8 @@ public partial class MonkeysViewModel : BaseViewModel
         this.connectivity = connectivity;
         this.geolocation = geolocation;
 
-        //CancellationToken cancellationToken = new();
-        //GetMonkeysAsync().WaitAsync(cancellationToken);
+        CancellationToken cancellationToken = new();
+        GetMonkeysAsync().WaitAsync(cancellationToken);
     }
 
     [RelayCommand]
